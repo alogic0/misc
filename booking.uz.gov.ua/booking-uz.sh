299aa5bcbb08f785398728cb5909303979a1b213
alogic0 <alogic0@gmail.com>
2017-06-25 04:07:04 +0300

    query booking.uz.gov.ua

--- booking.uz.gov.ua/booking-uz.sh
+++ booking.uz.gov.ua/booking-uz.sh
@@ -3,6 +3,7 @@
 ## Usage: $0 OD KOV 19.08
 
 YEAR=2017
+DT=19.08
 declare -A names
 names=([2218020]=Ковель
        [2208001]=Одесса
@@ -11,28 +12,38 @@ names=([2218020]=Ковель
 declare -A codes
 codes=([KOV]=2218020
        [OD]=2208001
-       [MINSK]=2100001)
+       [MIN]=2100001)
 
-if [ ! -z $3 ]
+if [[ -n $2 ]]
   then
     FROM=${codes[$1]}
     TO=${codes[$2]}
-    DATE=${3:-19.08}.$YEAR
+    DATE=${3:-$DT}.$YEAR
 fi
 if [ x$(basename $0) == x"booking-uz-kov-od.sh" ]
   then FROM=${codes[KOV]}
        TO=${codes[OD]}
-       DATE=${1:-19.08}.$YEAR
+       DATE=${1:-$DT}.$YEAR
   else if [ x$(basename $0) == x"booking-uz-od-kov.sh" ]
         then FROM=${codes[OD]}
              TO=${codes[KOV]}
-             DATE=${1:-19.08}.$YEAR
+             DATE=${1:-$DT}.$YEAR
        fi
   
 fi
 if [[ (-n $FROM) && (-n $TO) ]]
   then
+    TMP=$(mktemp /tmp/$(basename $0)-$(date +%Y-%m-%d-%H%M%S)-XXX)
     echo "On date $DATE ${names[$FROM]} - ${names[$TO]}"
-    wget -q -O - --post-data='station_id_from='$FROM'&station_id_till='$TO'&station_from=&station_till=&date_dep='$DATE'&time_dep=00%3A00&time_dep_till=&another_ec=0&search=' http://booking.uz.gov.ua/ru/purchase/search/
+    wget -q -O - --post-data='station_id_from='$FROM'&station_id_till='$TO'&station_from=&station_till=&date_dep='$DATE'&time_dep=00%3A00&time_dep_till=&another_ec=0&search=' http://booking.uz.gov.ua/ru/purchase/search/ | tee $TMP
+    if grep -q 'places' $TMP
+      then
+          d=$(cut -d '.' -f 1 <<<$DATE)
+          m=$(cut -d '.' -f 2 <<<$DATE)
+          Y=$(cut -d '.' -f 3 <<<$DATE)
+          echo
+          echo 'http://booking.uz.gov.ua/ru/?date='${Y}-${m}-${d}'&from='$FROM'&time=00%3A00&to='$TO'&url=train-list'
+    fi
+    rm $TMP
     echo
 fi
