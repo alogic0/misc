0d9bf0cfa1c1816a1b6cadb695bf5b793d25c990
alogic0 <alogic0@gmail.com>
2017-09-29 12:26:04 +0300

    Lenovo battery status

--- android/bat-state.sh
+++ android/bat-state.sh
@@ -1,4 +1,5 @@
 FN=/sys/class/power_supply/bq27441_battery/uevent
 echo $FN
-cat $FN |\
-  sed -ne '1,2p;10,11p;14,15p;16p'
+cat -n $FN |\
+  sed -n 's/=/ /;1,2p;10,11p;14,15p;16p' \
+  | while read i; do printf '%2s  %-31s  %15s\n' $i; done
