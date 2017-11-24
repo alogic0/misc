8b80c97d7fc461d39f53830a9750f97e76131d33
Oleg Tsybulskyi <alogic0@gmail.com>
2017-11-24 16:40:15 +0200

    Change to https

--- booking.uz.gov.ua/booking-uz.sh
+++ booking.uz.gov.ua/booking-uz.sh
@@ -8,7 +8,7 @@ declare -A names
 names=([2218020]=Ковель
        [2208001]=Одесса
        [2100001]=Минск) 
-
+:
 declare -A codes
 codes=([KOV]=2218020
        [OD]=2208001
@@ -31,18 +31,19 @@ if [ x$(basename $0) == x"booking-uz-kov-od.sh" ]
        fi
   
 fi
+
 if [[ (-n $FROM) && (-n $TO) ]]
   then
     TMP=$(mktemp /tmp/$(basename $0)-$(date +%Y-%m-%d-%H%M%S)-XXX)
     echo "On date $DATE ${names[$FROM]} - ${names[$TO]}"
-    wget -q -O - --post-data='station_id_from='$FROM'&station_id_till='$TO'&station_from=&station_till=&date_dep='$DATE'&time_dep=00%3A00&time_dep_till=&another_ec=0&search=' http://booking.uz.gov.ua/ru/purchase/search/ | tee $TMP
+    wget -q -O - --post-data='station_id_from='$FROM'&station_id_till='$TO'&station_from=&station_till=&date_dep='$DATE'&time_dep=00%3A00&time_dep_till=&another_ec=0&search=' https://booking.uz.gov.ua/ru/purchase/search/ | tee $TMP
     if grep -q 'places' $TMP
       then
           d=$(cut -d '.' -f 1 <<<$DATE)
           m=$(cut -d '.' -f 2 <<<$DATE)
           Y=$(cut -d '.' -f 3 <<<$DATE)
           echo
-          echo 'http://booking.uz.gov.ua/ru/?date='${Y}-${m}-${d}'&from='$FROM'&time=00%3A00&to='$TO'&url=train-list'
+          echo 'https://booking.uz.gov.ua/ru/?date='${Y}-${m}-${d}'&from='$FROM'&time=00%3A00&to='$TO'&url=train-list'
     fi
     rm $TMP
     echo
