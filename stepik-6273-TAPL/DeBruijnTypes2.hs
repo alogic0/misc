1b8b2e761ff21c11cd5010697b5e5514af819d12
alogic0 <alogic0@gmail.com>
2018-03-28 14:06:26 +0300

    typed calc

--- stepik-6273-TAPL/DeBruijnTypes2.hs
+++ stepik-6273-TAPL/DeBruijnTypes2.hs
@@ -14,6 +14,7 @@ data Term = Fls
           | Idx Int
           | Term :@: Term
           | Lmb Symb Type Term
+          | Let Symb Term Term
           deriving (Read,Show)
 
 instance Eq Term where
@@ -23,6 +24,7 @@ instance Eq Term where
   Idx m     == Idx m1      =  m == m1
   (u:@:w)   == (u1:@:w1)   =  u == u1 && w == w1
   Lmb _ t u == Lmb _ t1 u1 =  t == t1 && u == u1
+  Let _ u w == Let _ u1 w1 =  u == u1 && w == w1
   _         == _           =  False
 
 newtype Env = Env [(Symb,Type)]
@@ -35,6 +37,9 @@ shift val = shiftAbove 0 where
                    | otherwise  = Idx $ k + val
   shiftAbove cutoff (t1 :@: t2) = shiftAbove cutoff t1 :@: shiftAbove cutoff t2
   shiftAbove cutoff (Lmb sym tp t) = Lmb sym tp $ shiftAbove (succ cutoff) t 
+  shiftAbove cutoff (Let sym t1 t2) =
+    let [t1', t2'] = map (shiftAbove cutoff) [t1, t2]
+    in Let sym t1' t2'
   shiftAbove cutoff (If t1 t2 t3) =
     let [t1', t2', t3'] = map (shiftAbove cutoff) [t1, t2, t3]
     in If t1' t2' t3'
@@ -46,6 +51,9 @@ substDB j s (Idx k)
             | otherwise = Idx k
 substDB j s (t1 :@: t2) = substDB j s t1 :@: substDB j s t2
 substDB j s (Lmb sym tp t) = Lmb sym tp $ substDB (succ j) (shift 1 s) t
+substDB j s (Let sym t1 t2) =
+    let [t1', t2'] = map (substDB j s) [t1, t2]
+    in Let sym t1' t2'
 substDB j s (If t1 t2 t3) =
     let [t1', t2', t3'] = map (substDB j s) [t1, t2, t3]
     in If t1' t2' t3'
@@ -64,9 +72,12 @@ oneStep (If Fls _ t3) = Just t3
 oneStep (If t1 t2 t3) = do
   t1' <- oneStep t1
   return (If t1' t2 t3)
-oneStep (t1@(Lmb x _ t1') :@: t2)
+oneStep (t1@(Lmb _ _ t1') :@: t2)
   | isValue t2 = return $ shift (-1) $ substDB 0 (shift 1 t2) t1'
   | otherwise = oneStep t2 >>= \t2' -> return (t1 :@: t2')
+oneStep (Let sym t1 t2)
+  | isValue t1 = return $ substDB 0 t1 t2
+  | otherwise = oneStep t1 >>= \t1' -> return (Let sym t1' t2)
 oneStep (t1 :@: t2) = oneStep t1 >>= \t1' -> return (t1' :@: t2)
 oneStep _ = Nothing
 
@@ -84,7 +95,15 @@ whnf (cNot :@: Fls) == Tru
 whnf (Idx 0 :@: (cNot :@: Tru)) == (Idx 0 :@: (Lmb "x" Boo (If (Idx 0) Fls Tru) :@: Tru))
 oneStep (cNot :@: If cNot Tru Fls) == Nothing
 
+test1 = Let "x" Fls $ Lmb "y" (Boo :-> Boo) (Idx 0 :@: Idx 1)
+oneStep test1 == Just (Lmb "y" (Boo :-> Boo) (Idx 0 :@: Fls))
+test2 = Let "x" (If Tru Fls Tru) $ Lmb "y" (Boo :-> Boo) (Idx 0 :@: Idx 1)
+oneStep test2 == Just test1
+whnf test2 == Lmb "y" (Boo :-> Boo) (Idx 0 :@: Fls)
+
 -}
+
+
 
 infer :: Env -> Term -> Maybe Type
 infer env@(Env envLst) u = 
@@ -100,9 +119,11 @@ infer env@(Env envLst) u =
       return t2T
     (Idx n) -> return $ snd $ envLst !! n
     (Lmb nm xT t) -> do
-    --  let (Lmb nm xT t) = shift 1 tp
       tT <- infer (Env ((nm,xT):envLst)) $ t
       return $ xT :-> tT
+    (Let nm t1 t2) -> do
+      t1T <- infer env t1
+      infer (Env ((nm,t1T):envLst)) $ t2
     (t1 :@: t2) -> do
       t1T <- infer env t1
       t2T <- infer env t2
@@ -128,5 +149,8 @@ term = Idx 1 :@: Idx 0
 infer env term == Just Boo
 term = Idx 0 :@: Idx 1
 infer env term == Nothing
+
+test = Let "x" Fls $ Lmb "y" (Boo :-> Boo) (Idx 0 :@: Idx 1)
+infer0 test == Just ((Boo :-> Boo) :-> Boo)
 
 -}
