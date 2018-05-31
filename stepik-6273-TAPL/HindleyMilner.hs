0ef650bd879b29f2386103c128d0241ecd3aca5a
Oleg Tsybulskyi <alogic0@gmail.com>
2018-06-01 01:23:58 +0300

    Update HindleyMilner.hs

--- stepik-6273-TAPL/HindleyMilner.hs
+++ stepik-6273-TAPL/HindleyMilner.hs
@@ -9,15 +9,15 @@ data Type
   | Type :-> Type
   deriving (Eq, Show)
   
-  exts :: (Type -> Maybe Type) -> Type -> Maybe Type
-  exts s t =
-    case (s t) of
-      Nothing -> Just t
-      x       -> x
+exts :: (Type -> Maybe Type) -> Type -> Maybe Type
+exts s t =
+  case (s t) of
+    Nothing -> Just t
+    x       -> x
   
-  subst :: (Type -> Maybe Type) -> Type -> Maybe Type
-  subst s t =
-    case t of
-      (t1 :-> t2) -> (:->) <$> (subst s' t1) <*> (subst s' t2)
-      _           -> s' t
-    where s' = exts s
+subst :: (Type -> Maybe Type) -> Type -> Maybe Type
+subst s t =
+  case t of
+    (t1 :-> t2) -> (:->) <$> (subst s' t1) <*> (subst s' t2)
+    _           -> s' t
+  where s' = exts s
