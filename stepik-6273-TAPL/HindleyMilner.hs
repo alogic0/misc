97735f69f98f968dd8c77f32eafec5ae865c4733
Oleg Tsybulskyi <alogic0@gmail.com>
2018-06-01 01:22:15 +0300

    Create HindleyMilner.hs

---
+++ stepik-6273-TAPL/HindleyMilner.hs
@@ -0,0 +1,23 @@
+module HindleyMilner where
+
+type Symb = String
+
+infixr 3 :->
+
+data Type
+  = TVar Symb
+  | Type :-> Type
+  deriving (Eq, Show)
+  
+  exts :: (Type -> Maybe Type) -> Type -> Maybe Type
+  exts s t =
+    case (s t) of
+      Nothing -> Just t
+      x       -> x
+  
+  subst :: (Type -> Maybe Type) -> Type -> Maybe Type
+  subst s t =
+    case t of
+      (t1 :-> t2) -> (:->) <$> (subst s' t1) <*> (subst s' t2)
+      _           -> s' t
+    where s' = exts s
