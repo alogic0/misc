e97662ea7586e03c43ff85cd9bb530f1ceaa6736
alogic0 <alogic0@gmail.com>
2018-07-12 00:36:15 +0300

    mod for August

--- booking.uz.gov.ua/fast-paste.sh
+++ booking.uz.gov.ua/fast-paste.sh
@@ -3,4 +3,13 @@
 # Usage: $0 name.txt
 ## Paste by middle mouse lines from name.txt, press Enter for the next line
 
-for i in $(cat $1); do echo $i | xsel -i -b -p; echo $i; read a; done
+#for i in $(cat $1); do xsel -d; echo $i | xsel -i -b -s -p; echo $i; read a; done
+
+NL=$(wc -l < "$1")
+for i in $(seq 1 $NL); do
+  l=$(sed -n ${i}p "$1");
+  xsel -d
+  echo $l | cut -d ' ' -f 1 | xsel -i -b -s -p
+  echo $l
+  read -s a
+done
