36ac11a1b7c90d0a1ba76b49f3aec74a141ebcff
alogic0 <alogic0@gmail.com>
2019-05-15 00:37:05 +0300

    grep -E -> grep -P

--- booking.uz.gov.ua/booking-uz-ussr-mobile.sh
+++ booking.uz.gov.ua/booking-uz-ussr-mobile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-## Usage: $0 OD KOV 08-19
-##    or: $0 OD KOV 2018-08-19
-##    or: $0 OD KOV
+## Usage: $0 Одесса Ковель 2019-06-28
+##    or: $0 барановичи минск 08-19
 
 ## requires parsing JSON utility 'jl'
 ## https://github.com/chrisdone/jl
@@ -10,25 +9,8 @@
 YEAR=$(date +'%Y')
 DT=${3:-$(date +'%m-%d')}
 
-declare -A names
-names=([2218020]=Ковель
-       [2208001]=Одесса
-       [2100001]=Минск) 
-
-declare -A codes
-codes=([KOV]=2218020
-       [OD]=2208001
-       [MIN]=2100001)
-
-#if [[ -n $2 ]]
-#  then
-#    FROM=${codes[$1]}
-#    TO=${codes[$2]}
-#fi
-## wget -q -O - "https://booking.uz.gov.ua/ru/train_search/station/?term=$1"
-
-read STATION1 FROM <<< $(grep -iE "^${1}[ \t]*[[:digit:]]+$" express-asu.txt)
-read STATION2 TO <<< $(grep -iE "^${2}[ \t]*[[:digit:]]+$" express-asu.txt)
+read STATION1 FROM <<< $(grep -Pi "^$1\s+\d+$" express-asu.txt);
+read STATION2 TO <<< $(grep -Pi "^$2\s+\d+$" express-asu.txt);
 
 if [[ $(echo $DT | sed 's/\-/ /g' | wc -w) -eq 3 ]]
   then
@@ -41,22 +23,26 @@ ROUTE="${1}-${2}"
 # PZD='from='$FROM'&to='$TO'&time=00%3A00&train=084%D0%A8&get_tpl=1'
 PZQ="from=${FROM}&to=$TO"
 
+# echo "PZQ: $PZQ, ROUTE: $ROUTE"; exit
+
 if [[ (-n $FROM) && (-n $TO) ]]
   then
 
     TMP=$(mktemp /tmp/$(basename $0)-$(date +%Y%m%d%H%M%S)-XXX)
     TMP2=$(mktemp /tmp/$(basename $0)-$(date +%Y%m%d%H%M%S)-2-XXX)
 ## DEBUG
-    # echo wget -q -O - --post-data=\"${PZQ}\&date=$DATE\" https://booking.uz.gov.ua/ru/train_search/ \> $TMP
-    # wget -q -O - --post-data="${PZQ}&date=${DATE}" https://booking.uz.gov.ua/ru/train_search/ > $TMP
+     # echo wget -q -O - --post-data=\"${PZQ}\&date=$DATE\" https://booking.uz.gov.ua/ru/train_search/ \> $TMP
+     # wget -q -O - --post-data="${PZQ}&date=${DATE}" https://booking.uz.gov.ua/ru/train_search/ > $TMP
+     # cat $TMP
+## exit
     wget -q -O $TMP \
       --post-data="${PZQ}&date=${DATE}&time=00%3A00&get_tpl=1" https://booking.uz.gov.ua/ru/mobile/train_search/
     MSG=$(jl '\o -> if ( elem "warning" $ keys o.data) then o.data.warning else (map (\l -> [l.num, l.from.stationTrain, l.to.stationTrain]) o.data.list)' $TMP)
     [[ -n $MSG ]] && echo $MSG | jl id --lines | while read TR; do
       if [[ "$TR" =~ ^\[\" ]];
         then read PZNAME FROMST TOST <<< $(cut -d '"' -f2,4,6 --output-delimiter=' ' <<< "$TR");
-             read STATION1 FROM <<< $(grep -iE "^${FROMST}[ \t]*[[:digit:]]+$" express-asu.txt)
-             read STATION2 TO <<< $(grep -iE "^${TOST}[ \t]*[[:digit:]]+$" express-asu.txt)
+             read STATION1 FROM <<< $(grep -iP "^${FROMST}\s+\d+$" express-asu.txt);
+             read STATION2 TO <<< $(grep -iP "^${TOST}\s+\d+$" express-asu.txt);
              PZQ="from=${FROM}&to=$TO"
 ## DEBUG
              # echo $PZQ
