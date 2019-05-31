ae2010264971cfc711f77722855c0a13b3c59dc6
Oleg Tsybulskyi <otsybulskyi@luxoft.com>
2019-05-31 14:01:09 +0300

    snd-sms add, tickets for July 2019

---
+++ booking.uz.gov.ua/snd-sms.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "$1" | /usr/bin/mail 38068xxxxxxx@sms.kyivstar.net
--- booking.uz.gov.ua/tickets-cron-2019-07-14.sh
+++ booking.uz.gov.ua/tickets-cron-2019-07-14.sh
@@ -2,20 +2,22 @@
 
 export PATH=$PATH:$HOME/bin
 
-YR=2018
-DT=07-16
+YR=2019
+DT=07-14
 
 PNM='OD - KOV'
-PZD='to=2218020&from=2208001&time=00%3A00&train=084%D0%A8&get_tpl=1'
+PZD='from=2208001&to=2218020&time=00%3A00&train=058Ш&get_tpl=1'
 
 #PNM='KOV - OD'
 #PZD='from=2218020&to=2208001&time=00%3A00&train=084%D0%9B&get_tpl=1'
 
-[ -d $HOME/tmp/kov-od ] || mkdir $HOME/tmp/kov-od 2>/dev/null
+mkdir -p $HOME/tmp/kov-od 2>/dev/null
+
+[ -d $HOME/tmp/kov-od ] || { echo "Error in making /tmp/kov-od"; exit 1; }
 
 LOG=$HOME/tmp/kov-od/${DT}.txt
 
-[ -f $LOG ] || touch $LOG
+[ -f $LOG ] || touch $LOG;
 
 P_PRE=$(tac $LOG | egrep -m 1 'Плацкарт' | egrep -o '[[:digit:]]+')
 P_PRE=${P_PRE:-0}
@@ -34,6 +36,7 @@ TMP=$(mktemp $HOME/tmp/kov-od/$(basename $0)-$(date +%Y%m%d%H%M%S)-XXX)
 TMP2=$(mktemp $HOME/tmp/kov-od/$(basename $0)-$(date +%Y%m%d%H%M%S)-2-XXX)
 wget -q -O - --post-data=${PZD}'&date='${YR}-${DT} https://booking.uz.gov.ua/ru/mobile/train_wagons/ > $TMP
 echo
+# cat $TMP; echo; #debug
 cat $TMP | grep -Eo '"title":"[[:alpha:]]+","letter":".","free":[[:digit:]]+,"cost":[[:digit:]]+' \
           | cut -d ',' -f 1,3,4 | grep -v 'Люкс' | tee $TMP2
 P_NOW=$(cat $TMP2 | grep 'Плацкарт' | egrep -o '"free":[[:digit:]]+' | egrep -o '[[:digit:]]+')
@@ -43,8 +46,8 @@ K_NOW=${K_NOW:-0}
 if [[ $K_NOW -gt $K_PRE ]]; then MSG="$MSG""Купе: $K_NOW +$(($K_NOW - $K_PRE)) "; fi
 if [[ $P_NOW -gt $P_PRE ]]; then MSG="$MSG""Плацкарт: $P_NOW +$(($P_NOW - $P_PRE)) "; fi
 
-if [[ "$MSG" != "" ]]; then snd-sms.sh "$DT $PNM $MSG"; fi
-#if [[ "$MSG" != "" ]]; then echo "$DT $PNM $MSG"; fi
+# if [[ "$MSG" != "" ]]; then snd-sms.sh "$DT $PNM $MSG"; fi
+if [[ "$MSG" != "" ]]; then echo "$DT $PNM $MSG"; fi
 
 K_PRE=$K_NOW;
 P_PRE=$P_NOW;
@@ -54,3 +57,4 @@ P_PRE=$P_NOW;
 
 rm $TMP $TMP2
 echo
+
